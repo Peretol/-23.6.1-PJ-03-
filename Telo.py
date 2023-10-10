e387fbb7dd79a1f756815fce5a3dda786472ebc8
Peretol <123242621+Peretol@users.noreply.github.com>
2023-10-10 15:12:21 +0300

    Create Telo.py

---
+++ Telo.py
@@ -0,0 +1,41 @@
+import telebot
+from Config import keys, TOKEN
+from Extensions import ConvertionException, CryptoConverter
+
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать работу введите комманду боту в следующем формате:\n<Имя валюты, цену которой Вы хотите узнать> \
+<Имя валюты в которой надо узнать цену введенной валюты> \
+<Количество переводимой валюты>\Увидеть список всех доступных валют: /values'
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+
+        if len(values) != 3:
+            raise ConvertionException('Ввод параметров не верный.')
+
+        quote, base, amount = values
+        total_base = CryptoConverter.convert(quote, base, amount)
+    except ConvertionException as e:
+        bot.reply_to(message, f'Вы ОШИБЛИСЬ.\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+    else:
+        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        bot.send_message(message.chat.id, text)
+
+
+bot.polling()
